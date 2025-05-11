e2f251bedccdfb095b935f69f3473c1a46f1327a
IlyaKrn <ilyakornoukhov@gmail.com>
2025-05-12 00:07:22 +0300

    cli interface

--- src/main/java/com/ilyakrn/Main.java
+++ src/main/java/com/ilyakrn/Main.java
@@ -16,7 +16,29 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String prog = new String(Files.readAllBytes(Paths.get("/home/ilyakrn/IdeaProjects/ModelLangCompiler/main.prog")), StandardCharsets.UTF_8);
+        String filePath = "main.prog";
+        String compileFlag = "-i";
+        if (args.length > 0) {
+            switch (args[0]){
+                case "-i":
+                    compileFlag = "-i";
+                    break;
+                case "-l":
+                    compileFlag = "-l";
+                    break;
+                case "-p":
+                    compileFlag = "-p";
+                    break;
+                default:
+                    System.err.println("Invalid compiler flag '" + args[0] + "'\nallowed flags:\n'-i' - compile and interpret\n'-l' - print compiled program presentation\n'-p' - print POLIZ of program presentation\nalso you can set file to compile as second argument");
+                    return;
+
+            }
+        }
+        if (args.length > 1) {
+            filePath = args[1];
+        }
+        String prog = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
 
         Lexer lexer = new Lexer();
         Parser parser = new Parser();
@@ -30,10 +52,46 @@ public class Main {
 
             internalProgramPresentation = parser.analyze(internalProgramPresentation);
             System.out.println("PARSER COMPLETE SUCCESSFUL");
-            System.out.println("INTERPRETING PROGRAM...");
 
-            interpreter.interpret(internalProgramPresentation);
-            System.out.println("INTERPRETATION COMPLETE SUCCESSFUL");
+            if (compileFlag.equals("-l")) {
+                System.out.println(internalProgramPresentation);
+            }
+            if (compileFlag.equals("-p")) {
+                System.out.println("===================POLIZ===================");
+                for (int i = 0; i < internalProgramPresentation.getPolizTable().size(); i++) {
+                    String lexemeName = "";
+                    String tableName = "";
+                    switch (internalProgramPresentation.getPolizTable().get(i).getTableId()){
+                        case InternalProgramPresentation.serviceTableId:
+                            lexemeName = internalProgramPresentation.getServiceTable().get(internalProgramPresentation.getPolizTable().get(i).getLexId()).getLexeme();
+                            tableName = "serv";
+                            break;
+                        case InternalProgramPresentation.delimiterTableId:
+                            lexemeName = internalProgramPresentation.getDelimiterTable().get(internalProgramPresentation.getPolizTable().get(i).getLexId()).getLexeme();
+                            tableName = "delim";
+                            break;
+                        case InternalProgramPresentation.identifierTableId:
+                            lexemeName = internalProgramPresentation.getIdentifierTable().get(internalProgramPresentation.getPolizTable().get(i).getLexId()).getLexeme();
+                            tableName = "ident";
+                            break;
+                        case InternalProgramPresentation.numberTableId:
+                            lexemeName = internalProgramPresentation.getNumberTable().get(internalProgramPresentation.getPolizTable().get(i).getLexId()).getLexeme();
+                            tableName = "num ";
+                            break;
+                        case InternalProgramPresentation.polizPointerTableId:
+                            lexemeName = String.valueOf(internalProgramPresentation.getPolizPointerTable().get(internalProgramPresentation.getPolizTable().get(i).getLexId()).getPolizIndex());
+                            tableName = "point";
+                            break;
+                    }
+                    System.out.println(String.format("%s\t%s\t\t%s", i, tableName, lexemeName));
+                }
+            }
+            if (compileFlag.equals("-i")) {
+                System.out.println("INTERPRETING PROGRAM...");
+                interpreter.interpret(internalProgramPresentation);
+                System.out.println("INTERPRETATION COMPLETE SUCCESSFUL");
+            }
+
 
             System.out.println("====================END=====================");
         } catch (CompilerExternalException e){
